d0d066369ad2ce8452b3b3b0fccd8018c63b5ea9
Aman Shivpure <amanshivpureit19@acropolis.in>
2023-12-06 22:01:12 +0530

    chnage

---
+++ src/compo1.jsx
@@ -0,0 +1,12 @@
+function sum (){
+    return(
+        <>
+        <form>
+            <input>sum number 1</input>
+            <input>sum number 2</input>
+            <button>Sum</button>
+        </form>
+        </>
+    )
+}
+export default sum;
